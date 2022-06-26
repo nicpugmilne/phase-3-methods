a8fd67e6301b4c43c2d0cefdfa1b9fd79ace443a
Nicola Milne <nicolalouisemilne@gmail.com>
2022-06-26 08:49:30 -0600

    commit tree 74c7072fc9bf315f9230f0074c8ccbb3daabe5bd

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
 # Your code here!
+def greet_programmer
+  puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet "Naureen"
+greet "Jimmy"
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default "Naureen"
+greet_with_default
+
+def add (num1, num2)
+    num1 + num2
+end
+
+add(1,2)
+
+def halve(num)
+    return nil unless num.class == Integer
+    
+    num / 2
+  end
